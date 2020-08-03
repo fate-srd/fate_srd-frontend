af4a718ec53c60ecaf97de99298db85a9f29604f
Randy Oest <oest@amazingrando.com>
2020-08-02 20:13:57 -0400

    Footer

---
+++ components/01-components/footer/_footer.stories.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import template from './_footer.twig';
+import logo from '../../../images/logo-with-shadow.svg';
+import patreon from '../../../images/become_a_patron_button@2x.png';
+
+/**
+ * Storybook Definition.
+ */
+export default { title: 'Regions/Site Footer' };
+
+export const SiteFooter = () => (
+  <div dangerouslySetInnerHTML={{ __html: template({ logo, patreon }) }} />
+);
