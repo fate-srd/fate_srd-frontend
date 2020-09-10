435898e69d49574dd9ba412d2c56985a5bf284c7
Randy Oest <oest@amazingrando.com>
2020-09-10 18:39:25 -0400

    Aside WIP

--- components/01-components/nav-in-page/nav-in-page.js
+++ components/01-components/nav-in-page/nav-in-page.js
@@ -1,5 +1,8 @@
 Drupal.behaviors.navInPage = {
   attach(context) {
+    /**
+     * Small screen functionality.
+     */
     const navInPage = context.querySelector('.nav-in-page');
     const mobileToggle = context.querySelector('.nav-in-page__mobile-toggle');
     const body = context.querySelector('body');
@@ -10,5 +13,27 @@ Drupal.behaviors.navInPage = {
     };
 
     mobileToggle.addEventListener('click', handleNavInPage);
+
+    /**
+     * Large screen functionality
+     */
+    const sectionToggleButton = context.querySelectorAll(
+      '.nav-in-page__show-menu',
+    );
+    const handleSectionToggleButton = () => {
+      this.setAttribute('aria-expanded', 'taco');
+      //   if ( content.getAttribute('aria-hidden') === 'true' ) {
+      //     content.setAttribute('aria-hidden', 'false');
+      //     button.setAttribute('aria-expanded', 'true');
+      // } else {
+      //     content.setAttribute('aria-hidden', 'true');
+      //     button.setAttribute('aria-expanded', 'false');
+      // }
+
+      //   this.setAttribute('aria-expanded', 'false')
+      //   aria-expanded="false"
+    };
+
+    sectionToggleButton.addEventListener('click', handleSectionToggleButton);
   },
 };
