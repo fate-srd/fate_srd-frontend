05a5d430fc45a5d107906cf8f2e076e89683be58
Randy Oest <oest@amazingrando.com>
2020-10-17 15:41:11 -0400

    Patreon bar

--- components/01-components/header/_header.stories.js
+++ components/01-components/header/_header.stories.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 import template from './_header.twig';
 import logo from '../../../images/logo.svg';
+import patreonLogo from '../../../images/patreon-bar-logo.png';
 
 import './header';
+import '../patreon-bar/patreon-bar';
 
 /**
  * Storybook Definition.
@@ -11,5 +13,5 @@ import './header';
 export default { title: 'Regions/Site Header' };
 
 export const SiteHeader = () => (
-  <div dangerouslySetInnerHTML={{ __html: template({ logo }) }} />
+  <div dangerouslySetInnerHTML={{ __html: template({ logo, patreonLogo }) }} />
 );
