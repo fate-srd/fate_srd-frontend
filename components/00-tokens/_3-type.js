7f033a987337a6b5588d8b8d239351722d7ef32a
Randy Oest <oest@amazingrando.com>
2020-11-15 14:42:19 -0500

    Fonts

--- components/00-tokens/_3-type.js
+++ components/00-tokens/_3-type.js
@@ -1,6 +1,6 @@
 const sampleText = `<h1 class="page-title">Didn't melt fairer keepsakes since Fellowship elsewhere.</h1>
 <p>Woodlands payment Osgiliath tightening. Barad-dur follow belly comforts tender tough bell? Many that live deserve death. Some that die deserve life. Outwitted teatime grasp defeated before stones reflection corset seen animals Saruman's call?</p>
-<h2>Tad survive ensnare joy mistake courtesy Bagshot Row. <a id="anchor" class="anchor"></a></h2>
+<h2><span class="fate_font big">C</span> Tad survive ensnare joy mistake courtesy Bagshot Row. <a id="anchor" class="anchor"></a></h2>
 <p>Ligulas step drops both? You shall not pass! Tender respectable success Valar impressive unfriendly bloom scraped? Branch hey-diddle-diddle pony trouble'll sleeping during jump Narsil.</p>
 <h3>North valor overflowing sort Iáve mister kingly money?</h3>
 <p>Curse you and all the halflings! Deserted anytime Lake-town burned caves balls. Smoked lthilien forbids Thrain?</p>
